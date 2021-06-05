7a9dd2c504f50511468a757508d957f98f00c6c5
Nikita Merzlyakov <merzlyakov02@gmail.com>
2021-06-06 02:19:05 +0300

    pj revisions

--- src/ru/vsu/cs/graphs/Digraph.java
+++ src/ru/vsu/cs/graphs/Digraph.java
@@ -1,10 +1,4 @@
 package ru.vsu.cs.graphs;
 
-/**
- * Интерфейс для описания ориентированного графа (орграфа)
- */
 public interface Digraph extends Graph {
-    /**
-     * Интерфейс полностью совпадает с Graph
-     */
 }
